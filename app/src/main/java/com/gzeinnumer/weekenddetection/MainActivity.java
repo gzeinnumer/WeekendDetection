38e1bdccaed472794bfebf7e782d5ab5b4db5ab7
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-12-17 21:17:15 +0700

    commit

--- app/src/main/java/com/gzeinnumer/weekenddetection/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/weekenddetection/MainActivity.java
@@ -7,7 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.gzeinnumer.weekenddetection.helper.WeekendDetection;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,9 +42,70 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<String> dateNoWeekend = WeekendDetection.getDateRangeNoWeekend(weekend, start_date, end_date);
         Log.d(TAG, "libur: " + dateNoWeekend);
+
+        countWeekenInDate();
+    }
+
+    private void countWeekenInDate() {
+        String _3daysBefore = getCalculatedDate("yyyy-MM-dd", -4);
+        String today = getDateYMD();
+
+        int countWeekend = countWeekend(_3daysBefore, today);
     }
 
     private static final String TAG = "asdsafasfasdada";
 
+    public int countWeekend(String start_date, String end_date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        int count = 0;
+        try {
+            Date d1 = formatter.parse(start_date);
+            Date d2 = formatter.parse(end_date);
+            count = saturdaysundaycount(d1,d2);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
+        return count;
+    }
+
+
+
+    public int saturdaysundaycount(Date d1, Date d2) {
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(d1);
+
+        Calendar c2 = Calendar.getInstance();
+        c2.setTime(d2);
+
+        int sundays = 0;
+        int saturday = 0;
+
+        while (! c1.after(c2)) {
+            if (c1.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ){
+                saturday++;
+            }
+            if(c1.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
+                sundays++;
+            }
+
+            c1.add(Calendar.DATE, 1);
+        }
+
+        System.out.println("Saturday Count = "+saturday);
+        System.out.println("Sunday Count = "+sundays);
+        return saturday + sundays;
+    }
+
+    public String getDateYMD() {
+        return new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+    }
+
+    public String getCalculatedDate(String dateFormat, int days) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat s = new SimpleDateFormat(dateFormat);
+        cal.add(Calendar.DAY_OF_YEAR, days);
+        return s.format(new Date(cal.getTimeInMillis()));
+    }
 }
